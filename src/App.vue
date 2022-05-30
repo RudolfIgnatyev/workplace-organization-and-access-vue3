dbb0d845d7e229b56396ea7a35455e2be8984c8c
Rudolf Ignatyev <ignatiev.rudolf2012@yandex.ru>
2022-05-30 20:53:16 +0300

    feat: создать шапку сайта и основы главной страницы

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-row class="px-2 justify-space-between">
+        <v-btn elevation="1">
+          К главной странице
+        </v-btn>
+        <v-avatar color="primary" tile>
+          <v-icon dark>
+            mdi-account
+          </v-icon>
+        </v-avatar>
+      </v-row>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+    <v-footer absolute>
+      <v-row class="pa-2">
+        Организация рабочего места и доступ к ИР
+      </v-row>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -10,7 +29,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/HomePage.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card
+    tile
+    max-width="80vw"
+    class="mx-auto"
+  >
+    <v-card-content>
+      <v-list tile>
+        <v-list-subheader>
+          Шаблоны заявок:
+        </v-list-subheader>
+        <v-list-item
+          v-for="(item, i) in mainMenuList"
+          :key="`main-menu-item-${i}`"
+          :value="item"
+          active-color="primary"
+        >
+          <v-list-item-title v-text="item"></v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-card-content>
+    <v-card-actions>
+      <v-btn elevation="1">Мои заявки / Архив</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data: () => ({
+    mainMenuList: [
+      'Заявка на доступ в помещения Управления и создание ключа ЭЦП',
+      'Заявка об обеспечении АРМ',
+      'Заявка на доступ к ИР',
+      'Заявка на обеспечение канцелярскими товарами и мебелью',
+      'Заявка о включении в приказ на допуск к ИС',
+      'Заявка на проведение инструктажей'
+    ]
+  })
+}
+</script>
+
+<style scoped>
+</style>
