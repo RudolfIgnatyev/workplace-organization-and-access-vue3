e879aff3bc21817508791a1f2c88267501dd2f68
Rudolf Ignatyev <ignatiev.rudolf2012@yandex.ru>
2022-05-30 22:40:46 +0300

    Изменение перехода по каталогу в deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # переход в каталог сборки
-cd ./dist
+cd dist
 
 # если вы публикуете на пользовательский домен
 # echo 'www.example.com' > CNAME
@@ -22,4 +22,4 @@ git commit -m 'deploy'
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:rudolfignatyev/workplace-organization-and-access-vue3.git master:gh-pages
 
-cd ..
+cd -
