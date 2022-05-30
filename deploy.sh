49e27517f2b4430e7ba4bfe6c3dd5990d7b04959
Rudolf Ignatyev <ignatiev.rudolf2012@yandex.ru>
2022-05-30 22:21:19 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,11 @@ set -e
 npm run build
 
 # переход в каталог сборки
-cd dist
+cd ./dist
 
 # если вы публикуете на пользовательский домен
 # echo 'www.example.com' > CNAME
 
-git init
 git add -A
 git commit -m 'deploy'
 
@@ -20,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:rudolfignatyev/workplace-organization-and-access-vue3.git master:gh-pages
+git push
 
-cd -
+cd ..
