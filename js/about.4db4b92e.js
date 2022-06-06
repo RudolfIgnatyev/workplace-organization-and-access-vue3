bc2a22e0c26b4e639370504c7a1feb6621e08b2e
Rudolf Ignatyev <ignatiev.rudolf2012@yandex.ru>
2022-06-06 21:54:29 +0300

    Updates

--- js/about.4db4b92e.js
+++ js/about.4db4b92e.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkworkplace_organization_and_access_vue3"]=self["webpackChunkworkplace_organization_and_access_vue3"]||[]).push([[443],{6506:function(a,n,e){e.r(n),e.d(n,{default:function(){return l}});var c=e(3396);const r={class:"about"},s=(0,c._)("h1",null,"This is an about page",-1),u=[s];function t(a,n){return(0,c.wg)(),(0,c.iD)("div",r,u)}var o=e(89);const i={},_=(0,o.Z)(i,[["render",t]]);var l=_}}]);
-//# sourceMappingURL=about.08b202ac.js.map
+//# sourceMappingURL=about.4db4b92e.js.map
